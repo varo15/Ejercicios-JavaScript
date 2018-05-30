36e7272c162dbdc7c4668a2945442a600f44f613
varo15 <37835826+varo15@users.noreply.github.com>
2018-05-30 23:55:48 +0200

    Didactic game

---
+++ Juegos/Juego Didactico/js/script.js
@@ -0,0 +1,10 @@
+// $(function () {
+//   alert("funciona");
+
+function launchGame() {
+  var name = document.getElementById("name").value;
+  var age = parseInt(document.getElementById("age").value);
+  document.getElementById("landing").style.height = "0px";
+  document.getElementById("landing").style.width = "0px";
+
+}

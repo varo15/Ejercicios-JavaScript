c55170362675e6164e2d0f3c7fafaf7ec1a6efc8
varo15 <37835826+varo15@users.noreply.github.com>
2018-06-11 11:02:01 +0200

    Comienzo Breakout

--- Juegos/Juego Didactico/js/script.js
+++ Juegos/Juego Didactico/js/script.js
@@ -1,10 +1,13 @@
-// $(function () {
-//   alert("funciona");
-
 function launchGame() {
   var name = document.getElementById("name").value;
   var age = parseInt(document.getElementById("age").value);
-  document.getElementById("landing").style.height = "0px";
-  document.getElementById("landing").style.width = "0px";
-
+  if (name.length == 0) {
+    alert("Introduce un nombre valido");
+  } else if (age.length == 0) or (age < 5) {
+    alert("La edad minima es de 5 años");
+  } else {
+    //hace desaparecer el cuadrio #landing
+    document.getElementById("landing").style.height = "0px";
+    document.getElementById("landing").style.width = "0px";
+  }
 }

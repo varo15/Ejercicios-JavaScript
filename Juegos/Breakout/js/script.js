874d89b249e2fd68af4e5fc83075584bb1795ae4
varo15 <37835826+varo15@users.noreply.github.com>
2018-06-11 13:41:42 +0200

    Breakout v2
    Colisiones entre ladrillos y pelota

--- Juegos/Breakout/js/script.js
+++ Juegos/Breakout/js/script.js
@@ -18,6 +18,29 @@ var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+//declaramos las variables necesarias para los ladrillos
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = {
+      x: 0,
+      y: 0,
+      status: 1
+    };
+  }
+}
+
+// -------- FIN DECLARACION VARIABLES -------- //
+
 //creamos una funcion que dibja la bola
 function drawBall() {
   ctx.beginPath();
@@ -27,13 +50,32 @@ function drawBall() {
   ctx.closePath();
 }
 
-//Definimos la funcion con la que dibujaremos la paleta
+//creamos la funcion con la que dibujaremos la paleta
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+//creamos la funcion que dibuja los ladrillos
+function drawBricks() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
 }
 
 //creamos una funcion que ira dibujando y actualizandose cada 10 milisegundos
@@ -45,8 +87,14 @@ function draw() {
   //llamamos a la funcion que dibuja la paleta
   drawPaddle();
 
+  //llamamos la funcion que detecta las colisiones entre la pelota y los ladrillos
+  collisionDetection();
+
   //llamamos a la funcion para dibujar la bola
   drawBall();
+
+  //Llamamos a la funcion para dibujar los ladrillos
+  drawBricks();
 
   //actualizamos las variables 'dx' y 'dy'
   x += dx;
@@ -68,7 +116,7 @@ function draw() {
       document.location.reload();
     }
   }
-  
+
   //controlamos el movimiento de la paleta y sus colisiones
   if (rightPressed) {
     paddleX += 7;
@@ -102,6 +150,21 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.keyCode == 37) {
     leftPressed = false;
+  }
+}
+
+//Declaramos la funcion con la que detectamos las colisiones de los ladrillos y la pelota
+function collisionDetection() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
   }
 }
 
